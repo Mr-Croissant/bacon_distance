6f388c7e4221f49f1819a5d78a7dcf450d01ec94
Croiss <croiss@croiss.com>
2026-03-15 13:23:40 +0200

    git ignore and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Bacon Distance",
+    version="1.0",
+    description="bacon distance",
+    author="Ely",
+    packages=find_packages(),
+    install_requires=["pytest"],
+)
