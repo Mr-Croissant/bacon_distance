f04f8ddf08a678ce6d1b8c948841b8c4f25a9794
Croiss <croiss@croiss.com>
2026-03-18 19:42:46 +0200

    added final touches to consumer

--- bacon_consumer/bacon_consumer/consumer.py
+++ bacon_consumer/bacon_consumer/consumer.py
@@ -28,8 +28,13 @@ def send_update_to_server(ch, method, properties, body, endpoint):
     if response.status_code == 200:
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
+def send_new_movie(ch, method, properties, body):
+    send_update_to_server(ch, method, properties, body, NEW_MOVIE_ENDPOINT)
 
-channel.basic_consume(queue="new_movies", on_message_callback=send_update_to_server, arguments={"endpoint": NEW_MOVIE_ENDPOINT})
-channel.basic_consume(queue="new_actors", on_message_callback=send_update_to_server, arguments={"endpoint": NEW_ACTOR_ENDPOINT})
+def send_new_actor(ch, method, properties, body):
+    send_update_to_server(ch, method, properties, body, NEW_ACTOR_ENDPOINT)
+
+channel.basic_consume(queue="new_movies", on_message_callback=send_new_movie)
+channel.basic_consume(queue="new_actors", on_message_callback=send_new_actor)
 print("Started Consuming!")
 channel.start_consuming()
